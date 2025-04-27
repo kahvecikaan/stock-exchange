181e3f129bc92b0a46858b41ff8c62ff560d6902
Kaan Kahveci <kaankahveci29@outlook.com>
2025-04-27 19:05:32 +0300

    Add config file for external API migration

---
+++ src/main/java/com/stockexchange/stock_platform/config/AlpacaConfig.java
@@ -0,0 +1,20 @@
+package com.stockexchange.stock_platform.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "alpaca")
+@Getter
+@Setter
+public class AlpacaConfig {
+    private String apiKey;
+    private String apiSecret;
+    private String baseurl;
+    private String dataBaseUrl;
+    private String tradingBaseUrl;
+    private String wsBaseUrl;
+    private int maxRequestPerMinute = 200;
+}
